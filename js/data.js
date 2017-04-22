3fc5db9b95a9860f7fe1d2557a92fde6f798eb26
Ziwei <ziwei.liu.23@gmail.com>
2017-04-22 15:27:45 +1200

    Working Add/Edit Sim and Search

---
+++ js/data.js
@@ -0,0 +1,218 @@
+var exportArea;
+var importButton;
+var exportButton;
+
+function loadJSON(path, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        success(JSON.parse(xhr.responseText));
+      }
+      else {
+        error(xhr);
+      }
+    }
+  };
+  xhr.open('GET', path, true);
+  xhr.send();
+}
+
+
+function getScaleFreeNetwork(nodeCount) {
+  var nodes = [];
+  var edges = [];
+  var connectionCount = [];
+
+  // randomly create some nodes and edges
+  for (var i = 0; i < nodeCount; i++) {
+    nodes.push({
+      id: i,
+      label: String(i)
+    });
+
+    connectionCount[i] = 0;
+
+    // create edges in a scale-free-network way
+    if (i == 1) {
+      var from = i;
+      var to = 0;
+      edges.push({
+        from: from,
+        to: to
+      });
+      connectionCount[from]++;
+      connectionCount[to]++;
+    }
+    else if (i > 1) {
+      var conn = edges.length * 2;
+      var rand = Math.floor(Math.random() * conn);
+      var cum = 0;
+      var j = 0;
+      while (j < connectionCount.length && cum < rand) {
+        cum += connectionCount[j];
+        j++;
+      }
+
+
+      var from = i;
+      var to = j;
+      edges.push({
+        from: from,
+        to: to
+      });
+      connectionCount[from]++;
+      connectionCount[to]++;
+    }
+  }
+
+  return {nodes:nodes, edges:edges};
+}
+
+var randomSeed = 764; // Math.round(Math.random()*1000);
+function seededRandom() {
+  var x = Math.sin(randomSeed++) * 10000;
+  return x - Math.floor(x);
+}
+
+function getScaleFreeNetworkSeeded(nodeCount, seed) {
+  if (seed) {
+    randomSeed = Number(seed);
+  }
+  var nodes = [];
+  var edges = [];
+  var connectionCount = [];
+  var edgesId = 0;
+
+
+  // randomly create some nodes and edges
+  for (var i = 0; i < nodeCount; i++) {
+    nodes.push({
+      id: i,
+      label: String(i)
+    });
+
+    connectionCount[i] = 0;
+
+    // create edges in a scale-free-network way
+    if (i == 1) {
+      var from = i;
+      var to = 0;
+      edges.push({
+        id: edgesId++,
+        from: from,
+        to: to
+      });
+      connectionCount[from]++;
+      connectionCount[to]++;
+    }
+    else if (i > 1) {
+      var conn = edges.length * 2;
+      var rand = Math.floor(seededRandom() * conn);
+      var cum = 0;
+      var j = 0;
+      while (j < connectionCount.length && cum < rand) {
+        cum += connectionCount[j];
+        j++;
+      }
+
+
+      var from = i;
+      var to = j;
+      edges.push({
+        id: edgesId++,
+        from: from,
+        to: to
+      });
+      connectionCount[from]++;
+      connectionCount[to]++;
+    }
+  }
+
+  return {nodes:nodes, edges:edges};
+}
+
+//*Save and Load*//
+
+function addContextualInformation(elem, index, array) {
+    elem.id = index;
+    elem.connections = network.getConnectedNodes(index);  
+}
+
+
+function exportNetwork() {
+    clearOutputArea();
+    
+    //Export Nodes
+    var nodes = objectToArray(network.body.data.nodes._data);
+    nodes.forEach(addContextualInformation);
+    var exportNValue = JSON.stringify(nodes, undefined, 2);
+    exportNArea.value = exportNValue;
+    download(exportNValue, 'Sims_Ndata.txt', 'text/json'); //backup save
+    localStorage.setItem("node_data", exportNValue); // save the item 
+
+    //Export Edges
+    var edges = objectToArray(network.body.data.edges._data);
+    var exportEValue = JSON.stringify(edges, undefined, 2);
+    exportEArea.value = exportEValue;
+    download(exportEValue, 'Sims_Edata.txt', 'text/json'); //backup save
+    localStorage.setItem("edge_data", exportEValue); // save the item 
+}
+
+function download(text, name, type) {
+    var a = document.createElement("a");
+    var file = new Blob([text], {type: type});
+    a.href = URL.createObjectURL(file);
+    a.download = name;
+    a.click();
+}
+
+function importNetwork() {
+  
+    var inputNValue = localStorage.getItem("node_data"); // retrieve
+    var inputEValue = localStorage.getItem("edge_data"); // retrieve
+
+    if(inputNValue != null && inputEValue != null){
+        //console.log("inputValue", inputNValue, inputEValue)
+        var inputNData = JSON.parse(inputNValue);
+        var inputEData = JSON.parse(inputEValue);
+
+        nodesData = getNodeData(inputNData)
+
+        var data = {
+            nodes: nodesData,
+            edges: getEdgeData(inputEData)
+        }
+        network = new vis.Network(container, data, options);
+    }
+}
+
+function getNodeData(data) {
+    var networkNodes = [];
+    updateIds("refresh", null)
+
+    //ADD ATTRIBUTES HERE FOR LOADING NEW ELEMENT ATTRIBUTES
+    data.forEach(function(elem, index, array) {
+        networkNodes.push({id: elem.id, label: elem.label, x: elem.x, y: elem.y, title: elem.title, shape: elem.shape, image: elem.image, color: elem.color});
+        updateIds("add", elem.id);
+    });
+
+    return new vis.DataSet(networkNodes);
+}
+
+function getEdgeData(data) {
+    var networkEdges = [];
+
+    //ADD ATTRIBUTES HERE FOR LOADING NEW ELEMENT ATTRIBUTES
+     data.forEach(function(edge, index, array) {
+         networkEdges.push({from: edge.from, to: edge.to, arrows: 'to', title: edge.title, font: {align: 'middle'}});
+    });
+
+
+    return new vis.DataSet(networkEdges);
+}
+
+function objectToArray(obj) {
+    return Object.keys(obj).map(function (key) { return obj[key]; });
+}
+
---
+++ js/script.js
@@ -0,0 +1,310 @@
+
+function init() {
+    exportNArea = document.getElementById('nodeData');
+    exportEArea = document.getElementById('edgeData')
+    importButton = document.getElementById('import_button');
+    exportButton = document.getElementById('export_button');
+
+    draw();
+}
+
+var nodes = null;
+var edges = null;
+var network = null;
+
+var nodesData = [];
+
+
+// randomly create some nodes and edges
+var data = getScaleFreeNetwork(0);
+var network;
+var container;
+var options
+
+function destroy() {
+    if (network !== null) {
+    network.destroy();
+    network = null;
+    }
+}
+
+function draw() {
+    destroy();
+    nodes = [];
+    edges = [];
+
+    // create a network
+    container = document.getElementById('mynetwork');
+
+    options = {
+    manipulation: {
+        
+        addNode: function (data, callback) {
+        data.label = "Name"
+        data.title = ""
+        data.shape = "circularImage"
+        data.image = ""
+        data.id = "0"
+
+        // filling in the popup DOM elements
+        document.getElementById('operation').innerHTML = "Add Sim";
+        document.getElementById('node-id').value = data.id;
+        document.getElementById('node-label').value = data.label;
+        document.getElementById('saveButton').onclick = saveData.bind(this, data, callback, "add");
+        document.getElementById('cancelButton').onclick = clearPopUp.bind();
+        document.getElementById('search').style.display = 'none';
+        },
+        editNode: function (data, callback) {
+        
+        // filling in the popup DOM elements
+        document.getElementById('operation').innerHTML = "Edit Sim";
+        document.getElementById('node-id').value = data.id;
+        document.getElementById('node-label').value = data.label;
+        //document.getElementById('node-title').value = data.title;
+        document.getElementById('saveButton').onclick = saveData.bind(this, data, callback, "edit");
+        document.getElementById('cancelButton').onclick = clearPopUp.bind();
+        document.getElementById('search').style.display = 'none';
+        
+        },
+        addEdge: function (data, callback) {
+
+        document.getElementById('edge-title').style.display = 'none'; //close input after editing (show in vis.js)
+        data.title = document.getElementById('edge-title').value;
+        data.color = forest;
+        data.arrow = "to";
+        //data.font = {size: ".8em" };
+        callback(data)
+        },
+        editEdge: function (data, callback) {
+        data.arrow = "to";
+        document.getElementById('edge-title').style.display = 'none'; //close input after editing (show in vis.js)
+        data.title = document.getElementById('edge-title').value;
+        data.color = famColour;
+
+        callback(data)
+        }
+    }
+}; 
+    network = new vis.Network(container, data, options);
+}
+
+function clearPopUp() {
+    //document.getElementById('saveButton').onclick = null;
+    //document.getElementById('cancelButton').onclick = null;
+    document.getElementById('search').style.display = 'block';
+    document.getElementById('edge-title').style.display = 'none';
+    
+    console.log("data", data)
+}
+
+var ids = []
+function updateIds(action, id){
+    if(action == "refresh"){
+        ids = []
+    }
+    ids += [id]
+}
+
+function saveData(data,callback,change) {
+
+    if(change == "add"){
+        if(ids.length != 0){
+            var id = parseInt(ids[ids.length -1]) + 1
+            data.id = id       
+        }
+        ids += [parseInt(data.id)];
+    }
+
+    var circleColour = "#73ABC2"
+    var aspiration = ""
+    var traits = ""
+    var details = ""
+
+    if(document.getElementById("node-female").checked){
+        circleColour = "#FFA0B9"
+    }else if(document.getElementById("node-dead").checked){
+        circleColour = "#C1C1C1"
+    }
+
+    getFamily(document.getElementById("node-family").value)
+
+    aspSelect = document.getElementById("aspiration")
+    aspiration = getSelectedOptions(aspSelect);
+
+    traitSelect = document.getElementById("traits")
+    traits = getSelectedOptions(traitSelect);
+
+    var job = document.getElementById("node-job").value;
+
+    console.log("traits", traits)
+
+    if(traits.length == 1){
+    details = "<div class='popup'><img id='asp' src='" + aspiration[0].value + 
+                "'><img src='" + traits[0].value + 
+                "'><br><p>" + job + "</p></div>"
+    }
+
+    if(traits.length == 2){
+    details = "<div class='popup'><img id='asp' src='" + aspiration[0].value + 
+                "'><img src='" + traits[0].value + 
+                "'><img src='" + traits[1].value + 
+                "'><br><p>" + job + "</p></div>"
+    }
+
+    if(traits.length == 3){
+        details = "<div class='popup'><img id='asp' src='" + aspiration[0].value + 
+                    "'><img src='" + traits[0].value + 
+                    "'><img src='" + traits[1].value + 
+                    "'><img src='" + traits[2].value + 
+                    "'><br><p>" + job + "</p></div>"
+        console.log(details)
+    }
+    portrait = document.getElementById("portrait").files[0].name;
+
+    data.label = document.getElementById('node-label').value;
+    data.image = "assets/Sims/" + portrait
+
+    
+    data.title = details;
+
+    data.color = {background: circleColour , border: famColour }
+    data.color.highlight = {background: circleColour , border: famColour }
+
+    clearPopUp();
+    callback(data);
+    
+}
+
+function getSelectedOptions(select) {
+    var result = [];
+    var options = select.getElementsByTagName('option');
+    for (var i = 0; i < options.length; i++) {
+        if (options[i].selected)
+            result.push(options[i]);
+    };
+    return result;
+}
+
+function clearOutputArea() {
+        exportNArea.value = "";
+        exportEArea.value = "";
+    }
+
+function searchData(){
+    var resultsList = document.getElementById("results");
+    if(resultsList != null){
+        resultsList.innerHTML = "";
+    }
+    console.log("nodes", nodesData)
+    var query = document.getElementById("node-searchData").value;
+
+    var results = nodesData.get({
+        filter: function (item) {
+            if(item.label.includes(query)){
+                return (item.label.includes(query));
+            }
+            return (item.title.includes(query));
+        }
+    });
+
+    displayResults(results);
+}
+
+function displayResults(results){
+
+    console.log(results)
+    var resultsList = document.getElementById("results");
+    
+    for(var i=0; i<results.length; i++){
+        var resultsItem = document.createElement("li");
+        
+        var name = document.createElement("div");
+        var container = document.createElement("div");
+        var img = document.createElement("img");
+        var data = document.createElement("div");
+
+        name.innerHTML = results[i].label
+        img.src = results[i].image;
+        data.innerHTML = results[i].title;
+
+        resultsItem.appendChild(name);
+        name.setAttribute("id", "resultName"); // added line
+
+        container.appendChild(img);
+        img.setAttribute("style", "height: 60px;"); // added line
+        container.appendChild(data);
+        data.setAttribute("style", "left: 40%; position: absolute;");
+        container.setAttribute("id", "resultItemContainer"); // added line
+        resultsItem.appendChild(container);
+
+        resultsItem.setAttribute("id", "resultItem"); // added line
+        resultsList.appendChild(resultsItem);
+    }
+}
+
+var famColour = "#7A7A7A"
+/*Family Tree colours*/
+var swamp = "#51574a"; var forest = "#447c69"; var moss = "#74c493"; var peat = "#8e8c6d"; var cream = "#e4bf80";
+var lemon = "#e9d78e"; var rind = "#e2975d"; var apricot = "#f19670"; var coral = "#e16552"; 
+var jasper = "#c94a53"; var dusty = "#be5168"; var orchid = "#a34974"; var mulberry = "#993767";
+var plum = "#65387d"; var royal = "#4e2472"; var lavender = "#9163b6"; var pink = "#e279a3"; var candy = "#e0598b";
+var sea = "#7c9fb0"; var sky = "#5698c4"; var mint = "#9abf88"; 
+
+/*Autumn Colours*/
+var velvet = "#472F49"; var cyan = "#2F7F7C"; var navy = "#043049"; var space ="#1B2338"
+
+function getFamily(fam){
+    console.log("family", fam)
+    switch(fam){
+    case "Alenko":
+        famColour = royal;
+        break;
+    case "Dove":
+        famColour = lemon;
+        break;
+    case "Hathaway":
+        famColour = mulberry;
+        break;
+    case "Howard":
+        famColour = rind;
+        break;
+    case "Johnston":
+        famColour = moss;
+        break;
+    case "McKinnon":
+        famColour = sky;
+        break;
+    case "Mikimoto":
+        famColour = jasper;
+        break;
+    case "Mulligan":
+        famColour = pink;
+        break;
+    case "Norton":
+        famColour = mint;
+        break;
+    case "Powers":
+        famColour = forest;
+        break;
+    case "Rutherford":
+        famColour = space;
+        break;
+    case "Spirited":
+        famColour = swamp;
+        break;
+    case "St John":
+        famColour = peat;
+        break;
+    case "Strong":
+        famColour = coral;
+        break;
+    case "Stone":
+        famColour = lavender;
+        break;
+    default:
+        famColour = "#7A7A7A"
+    }
+}
+
+
+
